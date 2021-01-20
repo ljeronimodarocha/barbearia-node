d3639f902cc304b9151dca8b373266bfe9296e45
Lucas Rocha <ljeronimodarocha@gmail.com>
2021-01-20 01:09:30 -0300

    .

--- api/index.js
+++ api/index.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const routes = require('./routes')
 
+const cors = require('./headers')
+
 const app = express()
 
 const port = 3000
+app.use(cors);
 
 routes(app)
+
 
 app.listen(port, () => console.log(`servidor está rodando na porta ${port}`))
 
